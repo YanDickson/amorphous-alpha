b81d729259033d165d05c85284f98d2d2f8a05b9
Hevon Gordon <hevondgordon.professional@gmail.com>
2021-11-21 16:38:15 -0500

    vercel test

--- amorphous-app/pages/api/products/fetchAllProducts.js
+++ amorphous-app/pages/api/products/fetchAllProducts.js
@@ -1,11 +1,12 @@
-import products from '../../../data/products.json'
+import products from "../../../data/products.json";
 
 export const route = "/api/products/fetchAllProducts";
 export default function handler(req, res) {
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     res.status(200).json(products);
-  }
-  else {
-    res.status(500).json({error: 'This endpoint only accepts GET requests'})
+  } else {
+    res.status(500).json({ error: "This endpoint only accepts GET requests" });
   }
 }
+
+const q = 10;
